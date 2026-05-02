f2078b81213ee8ecf42f41021f88c19f2e6e7bb3
edgarmauricio123 <22620066@tlaxiaco.tecnm.mx>
2026-05-02 10:06:00 -0600

    CU-86ah6kzxv - Implementar petición POST de materias.

--- Frontend/src/services/MateriaService.js
+++ Frontend/src/services/MateriaService.js
@@ -1,20 +1,28 @@
 import axios from "axios";
 
-const API_URL ="http://localhost:8080/materias";
+const API_URL = "http://localhost:8080/materias";
 
-
+// GET todas
 export const obtenerMaterias = () => {
-  return axios.get(`${API_URL}/traer-materias`);
+  return axios.get(API_URL);
 };
 
-export const guardarMateria = (materia) =>{
-    return axios.post(`${API_URL}/insertar-materias`, materia);
+// GET por ID
+export const obtenerMateriaPorId = (id) => {
+  return axios.get(`${API_URL}/${id}`);
 };
 
+// POST
+export const guardarMateria = (materia) => {
+  return axios.post(API_URL, materia);
+};
+
+// PUT
 export const actualizarMateria = (id, materia) => {
-  return axios.put(`${API_URL}/editar-materias/${id}`, materia);
+  return axios.put(`${API_URL}/${id}`, materia);
 };
 
+// DELETE
 export const eliminarMateria = (id) => {
-  return axios.delete(`${API_URL}/eliminar-materias/${id}`);
+  return axios.delete(`${API_URL}/${id}`);
 };
